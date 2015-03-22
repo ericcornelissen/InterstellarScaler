d0dcadc74df55e28940ed71e3ce041447f5a7f93
Eric Cornelissen <ericornelissen@gmail.com>
2015-03-22 15:14:06 +0100

    First update
    - Some small changes, the opening screen is now a lot better, it just
    blends in with the overall interface.
    - Scaled the planets, not all planets are scaled equally. The smaller
    planets are all scaled to the size of earth. The bigger planets are a
    bit more complicated. I may provide a table with scaling information
    later...
    - Improved animations.
    - Added a menu (it's incredibly ugly atm) to quickly go to another
    planet.

--- script.js
+++ script.js
@@ -1,32 +1,16 @@
-var __IS_cur_planetID = 4;
+var __IS_cur_planetID = 4; // Earth
 
 var __IS_acc_Sun = 274.00;
 var __IS_acc_Mercury = 3.70;
 var __IS_acc_Venus = 8.88;
 var __IS_acc_Earth = 9.81;
 var __IS_acc_Moon = 1.63;
-
-function _Next() {
-	if (__IS_cur_planetID < 5) {
-		// Animate background
-		$("#background").animate({ "background-positionY": "-=100px" }, 1000);
-
-		// Animate planet
-		__IS_cur_planetID += 1;
-		_Planets()
-	}
-}
-function _Prev() {
-	if (__IS_cur_planetID > 1) {
-		// Animate background
-		$("#background").animate({ "background-positionY": "+=100px" }, 1000);
-
-		// Animate planet
-		__IS_cur_planetID -= 1;
-		_Planets()
-	}
-}
-
+var __IS_acc_Mars = 3.7;
+var __IS_acc_Jupiter = 24.9;
+var __IS_acc_Saturn = 10.50;
+var __IS_acc_Uranus = 9.00;
+var __IS_acc_Neptune = 11.20;
+var __IS_acc_Pluto = 0.77;
 
 function _setWeigth() {
 	var original = document.getElementById('weight').value;
@@ -35,46 +19,141 @@ function _setWeigth() {
 	var weightMercury = Math.round((__IS_acc_Mercury / __IS_acc_Earth) * original * 100) / 100;
 	var weightVenus = Math.round((__IS_acc_Venus / __IS_acc_Earth) * original * 100) / 100;
 	var weightMoon = Math.round((__IS_acc_Moon / __IS_acc_Earth) * original * 100) / 100;
+	var weightMars = Math.round((__IS_acc_Mars / __IS_acc_Earth) * original * 100) / 100;
+	var weightJupiter = Math.round((__IS_acc_Jupiter / __IS_acc_Earth) * original * 100) / 100;
+	var weightSaturn = Math.round((__IS_acc_Saturn / __IS_acc_Earth) * original * 100) / 100;
+	var weightUranus = Math.round((__IS_acc_Uranus / __IS_acc_Earth) * original * 100) / 100;
+	var weightNeptune = Math.round((__IS_acc_Neptune / __IS_acc_Earth) * original * 100) / 100;
+	var weightPluto = Math.round((__IS_acc_Pluto / __IS_acc_Earth) * original * 100) / 100;
 
 	$("#sun .weight").text(weightSun);
 	$("#mercury .weight").text(weightMercury);
 	$("#venus .weight").text(weightVenus);
 	$("#earth .weight").text(original);
 	$("#moon .weight").text(weightMoon);
+	$("#mars .weight").text(weightMars);
+	$("#jupiter .weight").text(weightJupiter);
+	$("#saturn .weight").text(weightSaturn);
+	$("#uranus .weight").text(weightUranus);
+	$("#neptune .weight").text(weightNeptune);
+	$("#pluto .weight").text(weightPluto);
 
-	$("#intro").hide();
+	$("ul li.up").removeClass("disabled");
+	$("ul li.down").removeClass("disabled");
+	$("ul li.menu").removeClass("disabled");
+	$("ul li.grid").removeClass("disabled");
+
+
+	$("#intro").addClass("hide");
+	$("#earth .planet-info").removeClass("hide");
 }
 
+function _Next() {
+	if (__IS_cur_planetID < 11) {
+		// Animate background
 
-function _Planets() {
-	switch(__IS_cur_planetID) {
-		case 1:
-			$("#mercury").removeClass("current").addClass("next")
+		// Animate planet
+		_setPlanets(__IS_cur_planetID + 1)
+	}
+}
+function _Prev() {
+	if (__IS_cur_planetID > 1) {
+		// Animate background
 
-			$("#sun").removeClass("next").removeClass("prev").addClass("current")
-			break;
-		case 2:
-			$("#sun").removeClass("current").addClass("prev")
-			$("#venus").removeClass("current").addClass("next")
+		// Animate planet
+		_setPlanets(__IS_cur_planetID - 1)
+	}
+}
 
-			$("#mercury").removeClass("next").removeClass("prev").addClass("current")
-			break;
-		case 3:
-			$("#mercury").removeClass("current").addClass("prev")
-			$("#earth").removeClass("current").addClass("next")
+function _setPlanets(ID) {
+	if (ID != __IS_cur_planetID) {
+		// Hide current planet
+		if (ID > __IS_cur_planetID) {
+			$(".current").removeClass("current").addClass("prev");
+		$("#background").stop().animate({ "background-positionY": "-=100px" }, 1000);
+		} else if (ID < __IS_cur_planetID) {
+			$(".current").removeClass("current").addClass("next");
+		$("#background").stop().animate({ "background-positionY": "+=100px" }, 1000);
+		}
 
-			$("#venus").removeClass("next").removeClass("prev").addClass("current")
-			break;
-		case 4:
-			$("#venus").removeClass("current").addClass("prev")
-			$("#moon").removeClass("current").addClass("next")
+		// Enable 
+		if (__IS_cur_planetID == 1) {
+			$("ul li.up").removeClass("disabled");
+		} else if (__IS_cur_planetID == 11) {
+			$("ul li.down").removeClass("disabled");
+		}
 
-			$("#earth").removeClass("next").removeClass("prev").addClass("current")
-			break;
-		case 5:
-			console.log('moon');
-			$("#moon").removeClass("next").addClass("current")
-			$("#earth").removeClass("current").addClass("prev")
-			break;
+		// Show new planet
+		switch(ID) {
+			case 1: // Sun
+				$("#mercury").removeClass("prev").addClass("next");
+
+				$("#sun").removeClass("prev").addClass("current");
+				$("ul li.up").addClass("disabled");
+				break;
+			case 2: // Mercury
+				$("#sun").removeClass("next").addClass("prev");
+				$("#venus").removeClass("prev").addClass("next");
+
+				$("#mercury").removeClass("next").removeClass("prev").addClass("current");
+				break;
+			case 3: // Venus
+				$("#mercury").removeClass("next").addClass("prev");
+				$("#earth").removeClass("prev").addClass("next");
+
+				$("#venus").removeClass("next").removeClass("prev").addClass("current");
+				break;
+			case 4: // Earth
+				$("#venus").removeClass("next").addClass("prev");
+				$("#moon").removeClass("prev").addClass("next");
+
+				$("#earth").removeClass("next").removeClass("prev").addClass("current");
+				break;
+			case 5: // Moon
+				$("#earth").removeClass("next").addClass("prev");
+				$("#mars").removeClass("prev").addClass("next");
+
+				$("#moon").removeClass("next").removeClass("prev").addClass("current");
+				break;
+			case 6: // Mars
+				$("#moon").removeClass("next").addClass("prev");
+				$("#jupiter").removeClass("prev").addClass("next");
+
+				$("#mars").removeClass("next").removeClass("prev").addClass("current");
+				break;
+			case 7: // Jupiter
+				$("#mars").removeClass("next").addClass("prev");
+				$("#saturn").removeClass("prev").addClass("next");
+
+				$("#jupiter").removeClass("next").removeClass("prev").addClass("current");
+				break;
+			case 8: // Saturn
+				$("#jupiter").removeClass("next").addClass("prev");
+				$("#uranus").removeClass("prev").addClass("next");
+
+				$("#saturn").removeClass("next").removeClass("prev").addClass("current");
+				break;
+			case 9: // Uranus
+				$("#saturn").removeClass("next").addClass("prev");
+				$("#neptune").removeClass("prev").addClass("next");
+
+				$("#uranus").removeClass("next").removeClass("prev").addClass("current");
+				break;
+			case 10: // Neptune
+				$("#uranus").removeClass("next").addClass("prev");
+				$("#pluto").removeClass("prev").addClass("next");
+
+				$("#neptune").removeClass("next").removeClass("prev").addClass("current");
+				break;
+			case 11: // Pluto
+				$("#neptune").removeClass("next").addClass("prev");
+
+				$("#pluto").removeClass("next").addClass("current");
+				$("ul li.down").addClass("disabled");
+				break;
+		}
+
+		__IS_cur_planetID = ID
+		$("#menu").removeClass("show");
 	}
 }
