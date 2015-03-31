7838ebadc6de793b38884f3b650bd0db8b830c16
Eric Cornelissen <ericornelissen@gmail.com>
2015-03-31 22:30:11 +0200

    Code update & new features
    I'll start with the new features and new things. First of all, the page
    now has a favicon (rescources\icons\favicon.png).
    Also, I added to icons to the menu page. You can click on them to select
    the text that will show up. The I is for information about the page,
    this is the text that was already there. The erlenmeyer flask will give
    you some information about the science behind this page (It explains
    that your mass isn't actually change, what is changing is the force).
    And last but certainly not least, along with the latest update to the
    InterstellarScaler.js UDF, you can now give a weight on every planet and
    the application will tell you what you weigh on other planets in
    relation to that specific planet.
    
    And then the updates to the code. I not only tried to improve the code,
    but I also tried to make the code more elegant. Especially the HTML and
    JavaScript should be much better now, since I already focussed on CSS in
    the previous commit.

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,55 @@
 var __IS_cur_planetID = 4; // Earth
 var __IS_cur_page = 0; // 0 = Intro; 1 = Planets; 2 = List
+var __IS_planetNames = new Array(12, "sun", "mercury", "venus", "earth", "moon", "mars", "ceres", "jupiter", "saturn", "uranus", "neptune", "pluto");
 
 $(document).ready(function() {
-	// Use the enter key to select your weight in the intro screen
-	$("#weight").keypress(function() { if (event.which == 13) { _setWeigth(); } });
+	// Listeners
+	$('input').keypress(function() { return event.charCode == 46 || event.charCode >= 48 && event.charCode <= 57; });
+
+	$('nav ul li.up').click(function() { _Prev(); });
+	$('nav ul li.down').click(function() { _Next(); });
+	$('nav ul li.grid').click(function() { _toggleGrid(); });
+
+	$("#weight").keypress(function() { if (event.which == 13) { _initialLoad(); } });
+	$("#explore").click(function() { _initialLoad(); });
+
+	$('#planets .planet-info h2, #planets .planet-info p').double_click(function () {
+		if (typeof __IS_cur_planetID == 'number' && __IS_cur_planetID > 0 && __IS_cur_planetID < 13) {
+			$('#' + __IS_planetNames[__IS_cur_planetID] + ' p').toggleClass('hide');
+			$('#' + __IS_planetNames[__IS_cur_planetID] + ' input').toggleClass('hide');
+		}
+	});
+	$("#planets .planet-info .input").keypress(function() { if (event.which == 13) {
+		_setWeight($('#planets #' + __IS_planetNames[__IS_cur_planetID] + ' .planet-info .input').val(),
+			__IS_planetNames[__IS_cur_planetID]);
+
+		$('#' + __IS_planetNames[__IS_cur_planetID] + ' p').removeClass('hide');
+		$('#' + __IS_planetNames[__IS_cur_planetID] + ' input').addClass('hide');
+		$('#planets #' + __IS_planetNames[__IS_cur_planetID] + ' .planet-info .input').val('');
+	} });
+	$('#planets .planet-info .go').click(function(){
+		_setWeight($('#planets #' + __IS_planetNames[__IS_cur_planetID] + ' .planet-info .input').val(),
+			__IS_planetNames[__IS_cur_planetID]);
+
+		$('#' + __IS_planetNames[__IS_cur_planetID] + ' p').removeClass('hide');
+		$('#' + __IS_planetNames[__IS_cur_planetID] + ' input').addClass('hide');
+		$('#planets #' + __IS_planetNames[__IS_cur_planetID] + ' .planet-info .input').val('');
+	});
+	
+	$('#btt-info').click(function(){
+		$('#science').addClass('hide'); $('#info').removeClass('hide');
+		$('#btt-info').addClass('disable'); $('#btt-science').removeClass('disable');
+	});
+	$('#btt-science').click(function(){
+		$('#info').addClass('hide'); $('#science').removeClass('hide');
+		$('#btt-science').addClass('disable'); $('#btt-info').removeClass('disable');
+	});
+
+	$('.tile').click(function(e) { _toggleGrid(); _setPlanets(parseInt(e.currentTarget.id)); });
+
 
 	// Scroll through planets when the users scrolls over the page
-	$("body").on("DOMMouseScroll mousewheel", function (event) {
+	$('body').on('DOMMouseScroll mousewheel', function (event) {
 		if (__IS_cur_page == 1) {
 			if (event.originalEvent.detail > 0 || event.originalEvent.wheelDelta < 0 ) {
 				_Next();
@@ -17,57 +60,68 @@ $(document).ready(function() {
 	});
 });
 
+function _initialLoad() {
+	if (_setWeight(document.getElementById('weight').value, 'earth') != -1) {
+		// Enable menu
+		$("ul li.up").removeClass("disable");
+		$("ul li.down").removeClass("disable");
+		$("ul li.grid").removeClass("disable");
+
+		// Hide intro
+		$("#intro").addClass("hide");
+		$("#earth").removeClass("intro-hide");
+		$("#earth .planet-info").removeClass("hide");
+
+		__IS_cur_page = 1; // Set current page
+	} else {
+		$("#weight").addClass("error"); // Make the text of the input red
+	}
+}
 function _toggleGrid() {
 	if (__IS_cur_page == 1) {
 		$("#planets").addClass("hide");
 		$("#menu").removeClass("hide");
-		$("ul li.up").addClass("disabled");
-		$("ul li.down").addClass("disabled");
+		$("ul li.up").addClass("disable");
+		$("ul li.down").addClass("disable");
 
 		__IS_cur_page = 2;
 	} else {
 		$("#planets").removeClass("hide");
 		$("#menu").addClass("hide");
 		if (__IS_cur_planetID != 1) {
-			$("ul li.up").removeClass("disabled");
+			$("ul li.up").removeClass("disable");
 		}
 		if (__IS_cur_planetID != 12) {
-			$("ul li.down").removeClass("disabled");
+			$("ul li.down").removeClass("disable");
 		}
 
 		__IS_cur_page = 1;
 	}
 }
 
-function _setWeigth() {
-	var iWeight = parseInt(document.getElementById("weight").value);
-	if (isNaN(iWeight) == false) { // 'iWeight' is NaN when the value is invalid
-		$("#sun .weight").text(_IS_convertWeight(iWeight, "earth", "sun"));
-		$("#mercury .weight").text(_IS_convertWeight(iWeight, "earth", "mercury"));
-		$("#venus .weight").text(_IS_convertWeight(iWeight, "earth", "venus"));
-		$("#earth .weight").text(iWeight);
-		$("#moon .weight").text(_IS_convertWeight(iWeight, "earth", "moon"));
-		$("#mars .weight").text(_IS_convertWeight(iWeight, "earth", "mars"));
-		$("#ceres .weight").text(_IS_convertWeight(iWeight, "earth", "ceres"));
-		$("#jupiter .weight").text(_IS_convertWeight(iWeight, "earth", "jupiter"));
-		$("#saturn .weight").text(_IS_convertWeight(iWeight, "earth", "saturn"));
-		$("#uranus .weight").text(_IS_convertWeight(iWeight, "earth", "uranus"));
-		$("#neptune .weight").text(_IS_convertWeight(iWeight, "earth", "neptune"));
-		$("#pluto .weight").text(_IS_convertWeight(iWeight, "earth", "pluto"));
+function _setWeight(nWeight, sBasePlanet) {
+	var weight = new ISweight(nWeight);
+	if (weight.success == true) {
+		_IS_setBasePlanet(sBasePlanet); // Set base planet to convert weights FROM
 
-		$("ul li.up").removeClass("disabled");
-		$("ul li.down").removeClass("disabled");
-		$("ul li.grid").removeClass("disabled");
-
-		$("#intro").addClass("hide");
-		$("#earth").removeClass("intro-hide");
-		$("#earth .planet-info").removeClass("hide");
-
-		__IS_cur_page = 1;
+		// Convert weights per planet
+		$("#sun .weight").text(weight.toSun());
+		$("#mercury .weight").text(weight.toMercury());
+		$("#venus .weight").text(weight.toVenus());
+		$("#earth .weight").text(weight.toEarth());
+		$("#moon .weight").text(weight.toMoon());
+		$("#mars .weight").text(weight.toMars());
+		$("#ceres .weight").text(weight.toCeres());
+		$("#jupiter .weight").text(weight.toJupiter());
+		$("#saturn .weight").text(weight.toSaturn());
+		$("#uranus .weight").text(weight.toUranus());
+		$("#neptune .weight").text(weight.toNeptune());
+		$("#pluto .weight").text(weight.toPluto());
 	} else {
-		$("#weight").addClass("error"); // Make the text of the input red
+		return -1;
 	}
 }
+
 function _setPlanets(ID) {
 	if (ID != __IS_cur_planetID) {
 		// Hide current planet
@@ -79,11 +133,15 @@ function _setPlanets(ID) {
 			$("body").stop().animate({ "background-positionY": "+=100px" }, 1000);
 		}
 
+		// Hide current planet input
+		$('#' + __IS_planetNames[__IS_cur_planetID] + ' p').removeClass('hide');
+		$('#' + __IS_planetNames[__IS_cur_planetID] + ' input').addClass('hide');
+
 		// Enable
 		if (__IS_cur_planetID == 1) {
-			$("ul li.up").removeClass("disabled");
+			$("nav ul li.up").removeClass("disable");
 		} else if (__IS_cur_planetID == 12) {
-			$("ul li.down").removeClass("disabled");
+			$("nav ul li.down").removeClass("disable");
 		}
 
 		// Show new planet
@@ -92,7 +150,7 @@ function _setPlanets(ID) {
 				$("#mercury").removeClass("prev").addClass("next");
 
 				$("#sun").removeClass("prev").addClass("current");
-				$("ul li.up").addClass("disabled");
+				$("nav ul li.up").addClass("disable");
 				break;
 			case 2: // Mercury
 				$("#sun").removeClass("next").addClass("prev");
@@ -158,13 +216,33 @@ function _setPlanets(ID) {
 				$("#neptune").removeClass("next").addClass("prev");
 
 				$("#pluto").removeClass("next").addClass("current");
-				$("ul li.down").addClass("disabled");
+				$("nav ul li.down").addClass("disable");
 				break;
 		}
 
 		__IS_cur_planetID = ID
-		$("#menu").removeClass("show");
 	}
 }
 function _Next() { if (__IS_cur_planetID < 12) { _setPlanets(__IS_cur_planetID + 1) } }
 function _Prev() { if (__IS_cur_planetID > 1) { _setPlanets(__IS_cur_planetID - 1) } }
+
+
+// Author:  Jacek Becela
+// Source:  http://gist.github.com/399624
+// License: MIT
+jQuery.fn.double_click = function(double_click_callback, timeout) {
+	return this.each(function(){
+		var clicks = 0, self = this;
+		jQuery(this).click(function(event){
+			clicks++;
+			if (clicks == 1) {
+				setTimeout(function(){
+					if(clicks != 1) {
+						double_click_callback.call(self, event);
+					}
+					clicks = 0;
+				}, timeout || 300);
+			}
+		});
+	});
+}
